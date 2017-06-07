d4135124a647a306348b9a5ca5a366584bca652d
mryanli <346754607@qq.com>
2017-06-07 20:25:07 +0800

    自动载入注册器
    自动载入注册器：spl_autoload_register

--- test.php
+++ test.php
@@ -1,6 +1,8 @@
 <?php
 //require "test1.php";
 //require "test2.php";
+spl_autoload_register("load1");
+spl_autoload_register("load2");
 
 test1::test();
 test2::test();
@@ -8,4 +10,9 @@ test2::test();
 function load1($class){
 
     require $class.".php";
+}
+
+function load2($class){
+
+   echo $class;
 }
